1827a8e0ebfcd0f2773d51f0aa3c95e432cd19aa
ikusa <joshuasetiawan@ymail.com>
2017-01-22 17:13:04 +0700

    Scirpt
    Script utk jquery bootstrap

--- SystemAkademis/resources/views/layouts/app.blade.php
+++ SystemAkademis/resources/views/layouts/app.blade.php
@@ -89,5 +89,8 @@
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
