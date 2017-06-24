91a9b641259709a90727c48b0620d788b1483dd3
genzzii <yosua.ian.sebastian@gmail.com>
2017-06-24 21:17:22 +0700

    Added tooltip and several eye candy

--- SystemBAA/app/Http/Controllers/PesertaController.php
+++ SystemBAA/app/Http/Controllers/PesertaController.php
@@ -29,40 +29,33 @@ class PesertaController extends Controller
 
      public function index(Request $request)
      {
+        $table = null;
+        $search_param = $request->input('id', 'null');
+        $biodata = \app\user::select('name')
+                   ->where('id', Auth::id())
+                   ->orderBy('id', 'desc')
+                   ->take(1)
+                   ->get();
 
-     $search_param = $request->input('id', 'null');
-     $biodata = \app\user::select('name')
-              ->where('id', Auth::id())
-              ->orderBy('id', 'desc')
-              ->take(1)
-              ->get();
-     if ($search_param!='null') {
+        if ($search_param!='null') {
 
-       $table = \app\student_course::select('id_mahasiswa','id_kelas','nim','program_studi')
-                ->join('mahasiswa', 'mahasiswa.id', '=', 'student_course.id_mahasiswa')
-                ->join('studi_program', 'mahasiswa.id_program_studi', '=', 'studi_program.id')
-                ->where('id_kelas',$search_param)
-                ->get();
+          $table = \app\student_course::select('id_mahasiswa','id_kelas','nim','program_studi')
+                   ->join('mahasiswa', 'mahasiswa.id', '=', 'student_course.id_mahasiswa')
+                   ->join('studi_program', 'mahasiswa.id_program_studi', '=', 'studi_program.id')
+                   ->where('id_kelas',$search_param)
+                   ->get();
+       }
 
-
-     }
-     $course = \app\course::select('*')
+        $course = \app\course::select('*')
               ->join('kelas', 'kelas.id_course', '=', 'course.id')
               ->where('kelas.id',$search_param)
               ->get();
-     Log::info('Special loop debug : '.$table);
 
-
-     if ($search_param=='null') {
-       $table = null;
-
+     return view('peserta', ['table' => $table,'biodata'=>$biodata,'course'=>$course,'idKelas'=>$search_param]);
      }
 
-     return view('peserta', ['table' => $table,'biodata'=>$biodata,'course'=>$course]);
-     }
-
-    public function create(Request $request)
-    {
+     public function create(Request $request)
+     {
 
       $term  = \app\term::select('*')
                ->get();
@@ -90,10 +83,29 @@ class PesertaController extends Controller
     }
     public function submit(Request $request)
     {
-      $input = $request->all();
-      unset($input['_token']);
-      DB::table('course')->insert($input);
+      $input = $request->except('_token','idKelas','inputTambah');
+      $idKelas = intval($request->input('idKelas'));
 
-      return redirect()->action('KRSController@index');
+      Log::info('Debug input: '.print_r($input, true));
+      Log::info('Debug idkelas: '.$idKelas);
+      foreach ($input as $key => $value) {
+         $idMahasiswa = \app\mahasiswa::select('mahasiswa.id')
+                        ->where('mahasiswa.nim', $value)
+                        ->first();
+
+         $idMahasiswa = $idMahasiswa['id'];
+         Log::info('Debug idmhs: '.$idMahasiswa);
+         Log::info('Debug idmhs type: '.gettype($idMahasiswa));
+         Log::info('Debug idkls type: '.gettype($idKelas));
+
+         /*
+         DB::table('student_course')->insert(
+            ['id_mahasiswa' => $idMahasiswa, 'id_kelas' => $idKelas]
+         );*/
+      }
+
+      return redirect()->action(
+         'PesertaController@index', ['id' => $idKelas]
+      );
     }
 }
--- SystemBAA/resources/views/peserta.blade.php
+++ SystemBAA/resources/views/peserta.blade.php
@@ -1,90 +1,178 @@
 @extends('layouts.master')
 
 @section('scripts')
-  <script type="text/javascript">
-  $(document).ready(function () {
+   <script type="text/javascript">
+   $(document).ready(function () {
 
-        $(".my_select_box").chosen({
-          no_results_text: "Oops, nothing found!",
-          width: "95%"
-        });
+         $(".my_select_box").chosen({
+            no_results_text: "Oops, nothing found!",
+            width: "95%"
+         });
 
-        $('#tambah').click(function() {
-           addRow(document.getElementById('banyakPeserta').value)
-        });
-  });
-  // Probably not needed, will be removed later.
-  function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+         $('#tambah').click(function() {
+            addRow(document.getElementById('banyakPeserta').value)
+         });
+
+         disableSubmit();
+   });
+
+   // Event listener for keeping value and clearing previous value
+   // in input tambah when typing.
+   banyakPeserta.addEventListener('focus', function () {
+      banyakPeserta.setAttribute('data-value', this.value);
+      this.value = '';
+   });
+   banyakPeserta.addEventListener('blur', function () {
+      if (this.value === '')
+         this.value = this.getAttribute('data-value');
+   });
+
+   // Probably not needed, will be removed later.
+   function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+         results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+   }
+
+   function disableSubmit() {
+      document.getElementById("submitPeserta").disabled = true;
+   }
+
+   function enableSubmit() {
+      document.getElementById("submitPeserta").disabled = false;
+   }
+
+   function changeBgColor(elementId, color) {
+      document.getElementById(elementId).style.backgroundColor = color;
+   }
+
+   function inputChanged(elementId) {
+      disableSubmit();
+      changeBgColor(elementId, YELLOWISH);
+   }
+
+   // Need realtime validation later
+   function banyakPesertaChanged() {
+      changeBgColor("banyakPeserta", YELLOWISH);
+      hideTooltip("tooltipTambah");
+   }
+
+   function showTooltip(tooltipId) {
+      var tooltip = document.getElementById(tooltipId);
+      tooltip.style.visibility = "visible";
+      tooltip.style.opacity = "1";
+   }
+
+   function hideTooltip(tooltipId) {
+      var tooltip = document.getElementById(tooltipId);
+      tooltip.style.visibility = "hidden";
+      tooltip.style.opacity = "0";
+   }
+
+   // Remove element based on given id.
+   function hapus(aku) {
+      console.log(aku);
+      $("#"+aku).remove();
+   }
+
+   // Need to check for each class later.
+   var MAX_PESERTA = 200;
+
+   // Color
+   var GREENISH = "#a9ff52",
+       PINKISH = "#f5baf1",
+       YELLOWISH = "#f6ff8b",
+       REDDISH = "#eb4760";
 
    // Add new rows in peserta table based on passed number.
    function addRow(banyak) {
-		var html = [];
-      var oldTotalPesertaBaru = document.getElementById("totalPesertaBaru").value;
-
+      var html = [];
+      var oldTotalPesertaBaru = document.getElementById("inputTambah").value;
       totalPesertaBaru = Number(oldTotalPesertaBaru) + Number(banyak);
-      document.getElementById("totalPesertaBaru").value = totalPesertaBaru;
 
-      var count = oldTotalPesertaBaru;
-      for (; count < totalPesertaBaru; count++) {
-         html.push("<tr><td id='peserta", count, "'>"
-          + "<input type='text' id='nim", count, "' name='nim", count, "'></td>"
-          + "<td name='nama", count, "' id='nama", count, "'></td>"
-          + "<td name='prodi", count, "' id='prodi", count, "'></td></tr>");
+      if (totalPesertaBaru >= MAX_PESERTA) {
+         alert("Class lebih dari batas maximal")
+         changeBgColor("banyakPeserta", PINKISH);
+
+      } else if (banyak > 0 && banyak <= 100) {
+         changeBgColor("banyakPeserta", GREENISH);
+         disableSubmit();
+         document.getElementById("inputTambah").value = totalPesertaBaru;
+
+         var count = oldTotalPesertaBaru;
+         for (; count < totalPesertaBaru; count++) {
+            html.push("<tr id='", count, "'><td id='peserta", count, "'>"
+               + "<input onchange=inputChanged('nim", count,"') class='form-control input-nim'"
+               + "type='text' id='nim", count, "' name='nim", count, "'></td>"
+               + "<td name='nama", count, "' id='nama", count, "'></td>"
+               + "<td name='prodi", count, "' id='prodi", count, "'></td>"
+               + "<td class='action-column'><button type='button' "
+               + "class='btn btn-info btn-removal' onclick=hapus('", count, "')"
+               + " id='kurang'>🗙</button></td></tr>");
+         }
+   		$('#peserta').append(html.join(''));
+
+      } else {
+         showTooltip("tooltipTambah")
+         changeBgColor("banyakPeserta", PINKISH);
       }
+   }
 
-		$('#peserta').append(html.join(''));
-  }
+   // Send ajax request to check nim from input in table mahasiswa
+   // and return necessary info.
+   $('#cek').click(function( event ) {
 
-  // Send ajax request to check nim from input in table mahasiswa
-  // and return necessary info.
-  $('#cek').click(function( event ) {
+      $.ajax({
+      url: '/kelas/peserta/cek',
+      type: 'post',
+      data: $('#daftarPeserta').serialize(),
+      dataType: 'json',
 
-    $.ajax({
-        url: '/kelas/peserta/cek',
-        type: 'post',
-        data: $('#daftarPeserta').serialize(),
-        dataType: 'json',
+      success: function( response ){
 
-        success: function( response ){
+         console.log($('#daftarPeserta').serialize());
 
-           var totalPeserta = document.getElementById("totalPesertaBaru").value;
-           // Loop for each nim inputted.
-           for (var count = 0; count < totalPeserta; count++) {
-             // Get each input field id to change color based on response
-             var inputField = 'nim'+count;
+         enableSubmit();
+         var totalPeserta = document.getElementById("inputTambah").value;
+         // Loop for each nim inputted.
+         //**************
+         //* Important
+         //* Must be changed to $.each loop
+         //* Current loop will broke if using remove, probably.
+         //**************
+         for (var count = 0; count < totalPeserta; count++) {
+            // Get each input field id to change color based on response
+            var inputField = 'nim'+count;
 
-             // Still need error checking for input field.
-             if (response.result[count].doesExist == true) {
-                // Change input field background color to greenish color when found.
-                document.getElementById(inputField).style.backgroundColor = "#a9ff52";
-                // Set the necessary info for each column on the table.
-                $('#nama'+count).text(response.result[count].nama);
-                $('#prodi'+count).text(response.result[count].program_studi);
-             } else {
-                // Set pink color on input field when input wasn't found on the DB.
-                document.getElementById(inputField).style.backgroundColor = "#f67da7";
-                $('#nama'+count).text("[ Student not found ]");
-                $('#prodi'+count).text("");
+            // Still need error checking for input field.
+            if (response.result[count].doesExist == true) {
+               // Change input field background color to greenish color when found.
+               changeBgColor(inputField, GREENISH);
+               // Set the necessary info for each column on the table.
+               $('#nama'+count).text(response.result[count].nama);
+               $('#prodi'+count).text(response.result[count].program_studi);
 
-             }
-           }
+            } else {
+               // Set pinkish color on input field when input wasn't found on the DB.
+               changeBgColor(inputField, PINKISH);
+               $('#nama'+count).text("[ Student not found ]");
+               $('#prodi'+count).text("");
 
-        },
-        error: function( response ){
-           // Do error handling later....
-           alert("Error while checking data.");
-        }
-    });
+               disableSubmit();
+            }
+         }
+      },
+         error: function( response ){
+            // Do error handling later....
+            alert("Error while checking data.");
+         }
+      });
 
-  });
+   });
 
 
 </script>
@@ -93,76 +181,148 @@
 @section('content')
 
 <style>
-// Ugly hax to keep consistent column size when the table is empty.
-// Still not working, later.
-th {
-   min-width: 25%;
-   max-width: 33%;
+   /* Temp? fix to keep consistent column size when the table is empty. */
+   th {
+      padding-left: 8px !important;
+      width: 25%;
+      min-width: 25%;
+      max-width: 30%;
+      }
+   td {
+      min-width: 25%;
+      max-width: 30%;
+      }
+
+   .action-column {
+      padding-left: 0px !important;
    }
-td {
-   min-width: 25%;
-   max-width: 33%;
+
+   .btn-removal {
+      background-color: #eb4760 !important;
+      border-color: #fff !important;
+      border-radius: 6px !important;
+   }
+
+   .form-control {
+      width: 100%;
+      padding: 10px;
+      margin: 0px;
+      box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      -webkit-box-sizing: border-box;
+   }
+
+   #banyakPeserta {
+      text-align:right;
+      display: inline;
+      width: 15em;
+      margin-right: 6px;
+   }
+
+   /* Configuring the padding inside input number. */
+   input::-webkit-outer-spin-button,
+   input::-webkit-inner-spin-button { margin-left: 10px;}
+
+   button {
+      margin-left: 5px;
+      display: inline;
+   }
+
+   /* Create tooltip if input outside of range. */
+   .tooltip {
+         position: relative;
+         bottom: 4px;
+         width: 210px;
+         height: 32px;
+         line-height: 20px;
+         padding: 6px;
+         font-size: 14px;
+         text-align: center;
+         color: rgb(255, 255, 255);
+         background: rgb(0, 0, 0);
+         border: 0px solid rgb(255, 255, 255);
+         border-radius: 8px;
+         text-shadow: rgba(0, 0, 0, 0.1) 1px 1px 1px;
+         box-shadow: rgba(0, 0, 0, 0.1) 1px 1px 2px 0px;
+         opacity: 0;
+         visibility: hidden;
+         transition: opacity 1s;
+   }
+
+   /* Little triangle below tooltip. */
+   .tooltip:after {
+         content: "";
+         position: absolute;
+         width: 0;
+         height: 0;
+         border-width: 10px;
+         border-style: solid;
+         border-color: #000 transparent transparent transparent;
+         top: 32px;
+         left: 95px;
    }
 </style>
 
 <div class="container">
-  <!-- page content -->
-  <div class="right_col" role="main">
+   <!-- page content -->
+   <div class="right_col" role="main">
       <!-- top tiles -->
 
       <!-- /top tiles -->
 
       <div class="row">
-          <div class="col-md-12 col-sm-12 col-xs-12">
-              <div class="dashboard_graph">
-
-
-                  <div class="clearfix"></div>
-              </div>
-          </div>
-
+         <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="dashboard_graph">
+               <div class="clearfix"></div>
+            </div>
+         </div>
       </div>
       <br>
-      <form id="daftarPeserta" action="/nilai/submit" method="post">
-        {{csrf_field()}}
-      <input type="number" id="banyakPeserta"
-         value=1 max=40 min=1 style="text-align:right;">
+      <form id="daftarPeserta" action="/kelas/peserta/submit" method="post">
+         {{csrf_field()}}
+
+      <span class="tooltip" id="tooltipTambah">Input between 1 - 100.</span>
+      <input type="number" onchange="banyakPesertaChanged()"
+         id="banyakPeserta" placeholder="Banyak peserta baru"
+         value="1" max="99" min="1"
+         class="form-control"/>
+
       <button type="button" class="btn btn-info" id="tambah">Tambah</button>
-      <input type="hidden" value="0" name="totalPesertaBaru" id="totalPesertaBaru">
+      <input type="hidden" value="0" name="inputTambah" id="inputTambah">
 
       <button type="button" class="btn btn-info" id="cek">Cek</button>
       <div style="float:right;">
-         <input type="submit" class="btn btn-success" value="Submit">
+         <input type="hidden" id="idKelas" name='idKelas' value='{{$idKelas}}' class="form-control" >
+         <input type="submit" id="submitPeserta" class="btn btn-success" value="Submit">
       </div>
 
 
       <div class="x_panel">
       <div class="x_content">
-          <table class="table table-hover" id="peserta">
-              <tbody>
-
-                <th>NIM</th>
-                <th>Nama</th>
-                <th>Program Studi</th>
-                @if ($table != null)
-                @foreach ($table as $key)
+         <table class="table table-hover" id="peserta">
+            <tbody>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Program Studi</th>
+            <th>Aksi</th>
+            @if ($table != null)
+               @foreach ($table as $key)
                   <tr>
-
-                    <td>{{$key->nim}}</td>
-                    <td>{{$key->nama}}</td>
-                    <td>{{$key->program_studi}}</td>
+                     <td>{{$key->nim}}</td>
+                     <td>{{$key->nama}}</td>
+                     <td>{{$key->program_studi}}</td>
                   </tr>
-                @endforeach
-                @endif
+               @endforeach
+            @endif
 
-              </tbody>
-          </table>
+            </tbody>
+         </table>
       </form>
       </div>
-    </div>
+      </div>
 
 
-  </div>
+   </div>
 </div>
 </div>
 <!-- /page content -->
